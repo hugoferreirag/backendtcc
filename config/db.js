26b8e1bf1a5500b318f93cbbf4f5002333788367
hugoferreirag <hugo.fersoft@gmail.com>
2019-10-28 23:23:39 -0300

    as

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
-// const config = require('../knexfile.js')
-// const knex = require('knex')(config)
+const environment = process.env.NODE_ENV || 'development';
+const config = require('../knexfile.js')[environment]
+const knex = require('knex')(config)
 
-// knex.migrate.latest([config])
-// module.exports = knex
+knex.migrate.latest([config])
+module.exports = knex
