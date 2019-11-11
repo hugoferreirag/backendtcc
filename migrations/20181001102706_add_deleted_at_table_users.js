7280612177adaaeaa56a3ea9091d3f617f2fdf26
hugoferreirag <hugo.fersoft@gmail.com>
2019-11-11 12:01:32 -0300

    as

--- migrations/20181001102706_add_deleted_at_table_users.js
+++ migrations/20181001102706_add_deleted_at_table_users.js
@@ -1,14 +1,12 @@
 
-exports.up = function(knex, Promise) {
-    return knex.schema.createTable('categories', table => {
-        table.increments('id').primary()
-        table.integer('parentId').references('id')
-            .inTable('users')
-        table.string('post',1000).notNull()
-        table.string('image',1000)
+exports.up = function (knex, Promise) {
+    return knex.schema.alterTable('users', table => {
+        table.timestamp('deletedAt')
     })
 };
 
-exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('categories')
+exports.down = function (knex, Promise) {
+    return knex.schema.alterTable('users', table => {
+        table.dropColumn('deletedAt')
+    })
 };
